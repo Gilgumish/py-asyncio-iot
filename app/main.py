5b95fcf4647fccec582830ef96957f1580b9249c
Dmytro Pukas <pukasdima2002@gmail.com>
2024-06-20 10:31:50 +0200

    flake8 fix

--- app/main.py
+++ app/main.py
@@ -27,15 +27,20 @@ async def main() -> None:
     hue_light_id, speaker_id, toilet_id = await asyncio.gather(
         service.register_device(hue_light),
         service.register_device(speaker),
-        service.register_device(toilet)
+        service.register_device(toilet),
     )
-
 
     # create a few programs
     wake_up_program = [
         service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
         service.send_msg(Message(speaker_id, MessageType.SWITCH_ON)),
-        service.send_msg(Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up")),
+        service.send_msg(
+            Message(
+                speaker_id,
+                MessageType.PLAY_SONG,
+                "Rick Astley - Never Gonna Give You Up",
+            )
+        ),
     ]
 
     sleep_program = [
